077fe984156b840f0b123f9b1c5dcd2919c5381c
sharifzubi <szubi@me.com>
2013-12-08 00:10:14 +0000

    finally hit rails_new

---
+++ final_project/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FinalProject::Application.config.secret_token = 'b19f5014c5ae459471f7b73aafbb53ee551998b9b58c7cc68bc469e84b33512c22cbd5a6027401437724821ac7c2fa8d11963a37b7ebc001257a6af6df84acaa'
