47ac7a57bb26d5ffc3d12210576948a4a4404afe
sharifzubi <szubi@me.com>
2013-12-09 15:02:56 +0000

    photos and memories controllers DONE

--- final_project/app/controllers/memories_controller.rb
+++ final_project/app/controllers/memories_controller.rb
@@ -1,37 +1,50 @@
 class MemoriesController < ApplicationController
 
   def index
-
+    @memories = Memory.all
   end
 
 
   def new
-
+    @photo = Photo.find params[:photo]
+    @memory = Memory.new
   end
 
 
   def create
-
+    @memory = Memory.new(params[:memory])
+    @memory.photo_id = params[:photo_id]
+    @memory.user = current_user
+    if @memory.save
+      redirect_to @memory.photo, notice: "Memory created successfully!"
+    else
+      flash[:alert] = "Memory NOT created!"
+      render :new
   end
 
 
   def show
-
+    @memory = Memory.find(params[:id])
   end
 
 
   def edit
-
+    @memory = Memory.find(params[:id])
   end
 
 
   def update
-
+    memory = Memory.find(params[:id])
+    memory.update_attributes(params[:memory])
+    redirect_to(memory)
   end
 
 
   def destroy
-
+    memory = Memory.find(params[:id])
+    memory.delete
+    redirect_to(memories_path), notice: "Memory successfully DELETED!!!"
+    #redirect_to(memory.photo)
   end
 
 
--- final_project/app/controllers/photos_controller.rb
+++ final_project/app/controllers/photos_controller.rb
@@ -1,22 +1,29 @@
 class PhotosController < ApplicationController
 
   def index
-
+    @photos = Photo.all
   end
 
 
   def new
-
+    @photo = Photo.new
   end
 
 
   def create
-
+    @photo = Photo.new params[:photo]
+    @photo.user = current_user
+    if @photo.save
+      session[:photo_id] = @photo.id
+      redirect_to @photo.user
+    else
+      render :new
+    end
   end
 
 
   def show
-
+    @photo = Photo.find(params[:id])
   end
 
 
@@ -26,12 +33,16 @@ class PhotosController < ApplicationController
 
 
   def update
-
+    photo = Photo.find(params[:id])
+    photo.update_attributes(params[:photo])
+    redirect_to(photo)
   end
 
 
   def destroy
-
+    photo = Photo.find(params[:id])
+    photo.delete
+    redirect_to(photos_path)
   end
 
 
