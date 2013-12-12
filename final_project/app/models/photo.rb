d9c23d22473339f7d2cb79a9b4a886b24060482b
sharifzubi <szubi@me.com>
2013-12-12 15:06:27 +0000

    fixes seeding users with image

--- final_project/app/models/photo.rb
+++ final_project/app/models/photo.rb
@@ -1,5 +1,5 @@
 class Photo < ActiveRecord::Base
-  attr_accessible :title, :image, :user_id, :created_at, :memories_attributes
+  attr_accessible :title, :image, :remote_image_url, :user_id, :created_at, :memories_attributes
   # acts_as_voteable
   # acts_as_taggable
 
